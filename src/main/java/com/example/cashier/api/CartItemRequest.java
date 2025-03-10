805d82260afc599f0c2c0b9f890c4f0e02b346a6
Magnus H <mangehl@gmail.com>
2025-03-10 22:08:59 +0100

    Remove DefaultRule class and related references, refactor a bit, activate validation

--- src/main/java/com/example/cashier/api/CartItemRequest.java
+++ src/main/java/com/example/cashier/api/CartItemRequest.java
@@ -10,7 +10,7 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 public record CartItemRequest(
         @Schema(description = "Quantity of the product being ordered", example = "2", requiredMode = REQUIRED)
         @Min(value = 1, message = "Quantity must be at least 1")
-        int quantity,
+        Integer quantity,
 
         @Schema(description = "Identifier of the product being ordered", requiredMode = REQUIRED, allowableValues = {"ARTICLE_1", "ARTICLE_2", "ARTICLE_3", "ARTICLE_4", "ARTICLE_5", "ARTICLE_6", "ARTICLE_7", "ARTICLE_8", "ARTICLE_9", "ARTICLE_10"})
         @NotNull(message = "Product is required")
--- src/main/java/com/example/cashier/api/CheckoutCartRequest.java
+++ src/main/java/com/example/cashier/api/CheckoutCartRequest.java
@@ -2,6 +2,7 @@ package com.example.cashier.api;
 
 import com.example.cashier.model.CartItem;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -10,5 +11,6 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 @Schema(description = "Request object for initiating a checkout")
 public record CheckoutCartRequest(
         @Schema(description = "List of product orders included in the checkout request", requiredMode = REQUIRED)
+        @Valid
         List<CartItemRequest> cartItems
 ) {}
